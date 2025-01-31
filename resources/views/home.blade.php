00fc64f824a703823fe0b8b3a4c141099fa9eadd
Arun Samal <it.arun1988@gmail.com>
2025-01-31 05:54:49 +0530

    changed ok to Hi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @php
     if (DB::connection()->getPdo()) {
-       echo "Successfully connected to my database ok=".DB::connection()->getDatabaseName();
+       echo "Successfully connected to my database Hi=".DB::connection()->getDatabaseName();
     }
 @endphp
